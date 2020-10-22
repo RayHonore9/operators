2f4d1c09d1687f67e6e6b3a69a7e799840559b71
RayHonore9 <raynaldhonore@gmail.com>
2020-10-22 19:10:34 -0400

    commit tree ec1aa83a0e1f9003df22a4e3df72efc7d1521ee2

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,21 @@
 def unsafe?(speed)
-
+    if speed > 60
+       return true
+    end 
+    if speed < 40 
+       return true 
+    else 
+       return false
+    end
 end
-
 
 
 def not_safe?(speed)
-	
-end
-	
-
-
+    #60 <=> 40 ? true : false
+    if speed <40 || speed >60
+      return true
+    else
+      return false
+    end
+         #binding.pry
+    end
